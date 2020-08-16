b84527863e09feb82d370996a91b34356c535b58
Ian Grubb <hi@iangrubb.com>
2020-08-16 19:01:14 -0400

    path update

--- lib/word_maze/gameplay/runtime_helpers/dictionary.ex
+++ lib/word_maze/gameplay/runtime_helpers/dictionary.ex
@@ -1,15 +1,10 @@
 defmodule WordMaze.Gameplay.Dictionary do
 
   def lookup(word) do
-    value =
-
-    File.stream!("#{File.cwd!()}/lib/word_maze/gameplay/runtime_helpers/word_list.txt")
+    File.stream!("./lib/word_maze/gameplay/runtime_helpers/word_list.txt")
     |> Enum.map(fn word -> String.trim(word) end)
     |> Enum.member?(String.upcase(word))
 
-    IO.puts(value)
-
-    value
   end
 
 end
