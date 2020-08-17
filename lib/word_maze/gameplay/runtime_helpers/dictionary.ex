4baa4f3d6142404d818315f3a4f36ed720065af1
Ian Grubb <hi@iangrubb.com>
2020-08-16 20:01:51 -0400

    moved word list to static folder

--- lib/word_maze/gameplay/runtime_helpers/dictionary.ex
+++ lib/word_maze/gameplay/runtime_helpers/dictionary.ex
@@ -2,9 +2,7 @@ defmodule WordMaze.Gameplay.Dictionary do
 
   def lookup(word) do
 
-    path = Path.expand("./lib/word_maze/gameplay/runtime_helpers/word_list.txt") |> Path.absname
-
-    File.stream!(path)
+    File.stream!("./assets/static/word_list.txt")
     |> Enum.map(fn word -> String.trim(word) end)
     |> Enum.member?(String.upcase(word))
 
