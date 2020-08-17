cfa4fa5fa6649bbee4afb682436ff7ef2636c052
Ian Grubb <hi@iangrubb.com>
2020-08-16 20:48:05 -0400

    updates path for deploy again

--- lib/word_maze/gameplay/runtime_helpers/dictionary.ex
+++ lib/word_maze/gameplay/runtime_helpers/dictionary.ex
@@ -2,7 +2,7 @@ defmodule WordMaze.Gameplay.Dictionary do
 
   def lookup(word) do
 
-    File.stream!("/app/lib/word_maze/word_maze-0.1.0/priv/static/word_list.txt")
+    File.stream!("/app/lib/word_maze-0.1.0/priv/static/word_list.txt")
     |> Enum.map(fn word -> String.trim(word) end)
     |> Enum.member?(String.upcase(word))
 
